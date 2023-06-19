bb8f3c2e29c04ec3f6ed6ee85247fa63cc48298e
Shokir <shokirsharofiddinov2909@gmail.com>
2023-06-19 10:54:25 +0500

    message

---
+++ validation/index.js
@@ -0,0 +1,9 @@
+const author_emial_pass = require("./author_email_pass.validator")
+const author = require("./author.validator")
+const category = require("./category.validator")
+
+module.exports = {
+    author_emial_pass,
+    author,
+    category
+}
